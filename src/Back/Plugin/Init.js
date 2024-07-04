49ef87582676b5d6f9b512548a508d5be9366a02
Alex Gusev <alex@flancer64.com>
2024-07-04 17:24:26 +0300

    lp-cust-9 Remove var/teq-app-install.zip in dev mode
    link to issue: flancer32/lp-cust#9

--- src/Back/Plugin/Init.js
+++ src/Back/Plugin/Init.js
@@ -1,17 +1,25 @@
 /**
  * The factory to create the plugin initialization function.
  *
- * @namespace TeqFw_Web_Source_Installer_Back_Plugin_Init
+ * @param {TeqFw_Web_Source_Installer_Back_Defaults} DEF
+ * @param {TeqFw_Core_Back_Config} config
  * @param {TeqFw_Web_Source_Installer_Back_Act_Create} actCreate
+ *
+ * @namespace TeqFw_Web_Source_Installer_Back_Plugin_Init
  */
 export default function Factory(
     {
+        TeqFw_Web_Source_Installer_Back_Defaults$: DEF,
+        TeqFw_Core_Back_Config$: config,
         TeqFw_Web_Source_Installer_Back_Act_Create$: actCreate,
     }
 ) {
 
     return async function act() {
+        /** @type {TeqFw_Core_Back_Plugin_Dto_Config_Local.Dto} */
+        const cfgCore = config.getLocal(DEF.MOD_CORE.SHARED.NAME);
+        const force = Boolean(cfgCore.devMode);
         // create archive with sources if not exists
-        await actCreate.act();
+        await actCreate.act({force});
     };
 }
